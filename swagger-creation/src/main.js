188cdb37132fa4bf921f8f2c6e8e36aa2f48cf97
IzzuddinAsroff <127574901+IzzuddinAsroff@users.noreply.github.com>
2023-06-21 04:02:22 +0800

    Update main.js

--- swagger-creation/src/main.js
+++ swagger-creation/src/main.js
@@ -1,5 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import _ from 'lodash';    
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+
+Object.defineProperty(Vue.prototype, '$_', { value: _ });
+
 
 Vue.config.productionTip = false
 
